d3177bed94d1223ef4bf5f5f6427174d02d0df1c
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-10-01 11:12:32 +0530

    update student profile page

--- src/pages/StudentProfile.js
+++ src/pages/StudentProfile.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import '../style/studentprofile.css'
+import '../style/studentprofile.css';
 import StudentCard from '../components/StudentCard';
 
-const studentData = {
-  name: 'John Doe',
-  photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
-};
+const studentData = [
+  {
+    name: 'John Doe',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'Jane Smith',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'John Doe',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'Jane Smith',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'John Doe',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'Jane Smith',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'John Doe',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+  {
+    name: 'Jane Smith',
+    photoUrl: 'https://img.freepik.com/premium-photo/young-student-caucasian-woman-isolated-white-background-pointing-side-present-product_1368-289762.jpg?w=2000',
+  },
+];
 
 class StudentProfile extends React.Component {
-
-    render() { 
-        return (
-          <>
-            <StudentCard student={studentData}/>
-          </>
-        );
-    }
+  render() {
+    return (
+      <div style={{display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent:'center',
+        margin: '10px'}}>
+        {studentData.map((student, index) => (
+          <StudentCard key={index} student={student} />
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default StudentProfile;
