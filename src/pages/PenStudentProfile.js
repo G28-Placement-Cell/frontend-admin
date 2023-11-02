159d50183044c4138a6b20af4cf2802492e62d6d
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-11-03 01:14:30 +0530

    connect penStudProfile to backend

--- src/pages/PenStudentProfile.js
+++ src/pages/PenStudentProfile.js
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../style/studentprofile.css";
 import PendingCard from "../components/PendingCard";
-import { useEffect } from "react";
-
 
 function StudentProfile() {
-
   const [studentData, setStudentData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -17,14 +14,17 @@ function StudentProfile() {
         'content-type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('token')}`
       },
-    }).then((res) => res.json()).then((data) => {
-      console.log(data);
-      setStudentData(data.penstudent);
-      setLoading(false);
-    }).catch((err) => {
-      console.log(err);
-      setLoading(false);
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setStudentData(data.penstudent);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -36,9 +36,15 @@ function StudentProfile() {
         margin: "10px",
       }}
     >
-      {studentData.map((student, index) => (
-        <PendingCard key={index} student_company={student} />
-      ))}
+      {loading ? (
+        <p>Loading...</p>
+      ) : studentData && studentData.length > 0 ? (
+        studentData.map((student, index) => (
+          <PendingCard key={index} student_company={student} />
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
     </div>
   );
 }
