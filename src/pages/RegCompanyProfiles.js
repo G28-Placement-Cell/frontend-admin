14fe4fe2e323400150add7250fd26c2fbfd231dc
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-11-03 01:14:52 +0530

    connect RegCompProfile to backend

--- src/pages/RegCompanyProfiles.js
+++ src/pages/RegCompanyProfiles.js
@@ -1,31 +1,46 @@
 import AcceptedCard from "../components/AcceptedCard";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../style/studentprofile.css";
 
-const regCompanyData = [
+function RegCompanyProfile() {
+  const [regCompanyData, setRegCompanyData] = useState([]);
 
-];
+  useEffect(() => {
+    fetch("http://localhost:8000/api/company/getregcompany", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setRegCompanyData(data.company);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        margin: "10px",
+      }}
+    >
+      {regCompanyData && regCompanyData.length > 0 ? (
+        regCompanyData.map((company, index) => (
+          <AcceptedCard key={index} student_company={company} />
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
+    </div>
+  );
+}
 
-function regCompanyProfile() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          margin: "10px",
-        }}
-      >
-        {companyData.map((company, index) => (
-          <AcceptedCard
-            key={index}
-            student_company={company}
-          />
-        ))}
-      </div>
-    );
-  }
-  
-  export default regCompanyProfile;
-  
+export default RegCompanyProfile;
