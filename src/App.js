cc447aadcad044273c1f5614aceaf8f16a7e1d2c
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-11-03 18:48:23 +0530

    add routes for new pages

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import StudentProfile from './pages/PenStudentProfile';
-import AnnouncementSection from './pages/AnnouncementSection';
+import AnnouncementSectionStudent from './pages/AnnouncementSectionStudent';
+import AnnouncementSectionCompany from './pages/AnnouncementSectionCompany';
 import ChangePassword from './pages/ChangePassword';
 import RegStudentProfile from './pages/RegStudentProfile';
 import PenCompanyProfile from './pages/PenCompanyProfile';
 import AdminLogin from './pages/AdminLogin';
 import { ToastContainer } from 'react-toastify';
 import RegCompanyProfile from './pages/RegCompanyProfiles';
+import AddAnnouncement from './pages/AddAnnouncement';
 
 function App() {
 
@@ -29,9 +31,10 @@ function App() {
             <Route path='/RegstudentProfiles' element={<RegStudentProfile />} />
             <Route path='/PencompanyProfiles' element={<PenCompanyProfile />} />
             <Route path='/RegcompanyProfiles' element={<RegCompanyProfile />} />
-            <Route path='/StudentAnnouncements' element={<AnnouncementSection />} />
-            <Route path='/CompanyAnnouncements' element={<AnnouncementSection />} />
+            <Route path='/StudentAnnouncements' element={<AnnouncementSectionStudent />} />
+            <Route path='/CompanyAnnouncements' element={<AnnouncementSectionCompany />} />
             <Route path='/changepassword' element={<ChangePassword />} />
+            <Route path='/addAnnouncement' element={<AddAnnouncement />} />
             <Route path='/logout' />
           </Routes>
         </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import theme from './theme';
 import store from './store';
 import { Provider } from 'react-redux';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
