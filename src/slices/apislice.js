029ea46c60abb784e2fd55b55b4a8d3c41a067fb
LEARNER17184 <202101497@daiict.ac.in>
2023-11-17 18:29:16 +0530

    links-final-done-admin-side

--- src/slices/apislice.js
+++ src/slices/apislice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://back-end-production-ee2f.up.railway.app/' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://back-end-production-3140.up.railway.app/' });
 
 export const apislice = createApi({
     baseQuery,
